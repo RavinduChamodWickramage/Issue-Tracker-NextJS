2cfe51119f2301ec9d5e00f248c2dfa02c738a6d
Ravindu Chamod Wickramage <ravinduchamod1@gmail.com>
2025-03-11 21:44:16 +0530

    chore[radix]: set up Radix UI with theme and styles

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import { Button } from "@radix-ui/themes";
+import React from "react";
 
 const IssuesPage = () => {
   return (
     <div>
-      Issues Page
+      <Button>New Issue</Button>
     </div>
-  )
-}
+  );
+};
 
-export default IssuesPage
+export default IssuesPage;
