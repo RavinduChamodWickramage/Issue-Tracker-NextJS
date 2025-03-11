deb00ac5e4f22df11ff6518b95a753c708ee68a0
Ravindu Chamod Wickramage <ravinduchamod1@gmail.com>
2025-03-11 13:32:27 +0530

    style[navbar]: enhance NavBar with Tailwind CSS styles and React Icons

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -18,9 +23,20 @@ const NavBar = () => {
           <Link
             key={link.href}
             href={link.href}
-            className="text-zinc-500 hover:text-zinc-900 transition-colors"
+            className={
+              link.href === currentPath
+                ? "text-green-700 hover:text-green-900 group relative transition-colors duration-300"
+                : "text-zinc-500 hover:text-zinc-900 group relative transition-colors duration-300"
+            }
           >
             {link.label}
+            <span
+              className={`absolute left-0 bottom-0 h-0.5 transition-all duration-300 ${
+                link.href === currentPath
+                  ? "w-full bg-green-700"
+                  : "w-0 group-hover:w-full bg-zinc-900"
+              }`}
+            ></span>
           </Link>
         ))}
       </ul>
