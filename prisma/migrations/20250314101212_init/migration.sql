52e271fa5d723b35e10acbe36d00847f532d8e57
Ravindu Chamod Wickramage <ravinduchamod1@gmail.com>
2025-03-14 16:30:17 +0530

    chore[database]: update schema and apply new migrations

---
+++ prisma/migrations/20250314101212_init/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the `sessions` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `sessions` DROP FOREIGN KEY `sessions_userId_fkey`;
+
+-- AlterTable
+ALTER TABLE `user` MODIFY `password` VARCHAR(255) NOT NULL;
+
+-- DropTable
+DROP TABLE `sessions`;
+
+-- CreateTable
+CREATE TABLE `Session` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `sessionToken` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Session_sessionToken_key`(`sessionToken`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Session` ADD CONSTRAINT `Session_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
